505c7519037638335de8c91eb6aaa365294af710
yoyo <yotam-ra.amrani@mail.huji.com>
2022-10-31 12:40:10 +0200

    initializing the environment

--- crawler.py
+++ crawler.py
@@ -1,2 +1,19 @@
+import sys
 
 
+def main():
+    args = sys.argv[1:]
+    print(args)
+
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+def verify_url_path(url):
+    """"""
+    pass
+def verify_input_arg():
+    pass
