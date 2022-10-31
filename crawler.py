edd93d2b607a76a372f8fe1da7876029969c50f4
yoyo <yotam-ra.amrani@mail.huji.com>
2022-10-31 14:59:58 +0200

    editting the main BFS method

--- crawler.py
+++ crawler.py
@@ -19,37 +19,37 @@ useful Links:
 """
 
 
-# TODO: create a class of a web page to crawl?
 # TODO: mark them if we have visited the page
 
-# TODO: open web page and extract images
 
-
-class ImageNode:
-    def __init__(self, source_url, image_url,depth):
+class PageNode:
+    def __init__(self, source_url,depth):
         self._url = source_url
         self._depth = depth
-        self._image_url = image_url
 
 
 def main():
-    args = sys.argv[1:]
-    # TODO: add test for the input args
-    print(args)
-    url = "https://www.geeksforgeeks.org/"
+    args = sys.argv[1:]  # TODO: add test for the input args
+
+    # Init the url and depth
+    url = args[0]
+    depth = int(args[1])
+
+    # Init  local files
+    json_file_path = "D:/Interviews/dataloop/results.json"
+    init_results_file(json_file_path)  # Init the results file
+
+    # run th scrapping process:
     images = find_page_images("https://www.geeksforgeeks.org/")
     urls = find_page_urls(url)
 
-    # Init the results file:
-    # init_results_file("D:/Interviews/dataloop/results.json")
-    # init_results_file("../results.json")
-    init_results_file("D:/results.json")
+    # run the main crawling function:
+    crawl_page(url, depth, json_file_path)
 
+    # Testing section:
     print("IMAGES: ")
-    for img in images:
-        write_json(img,"D:/results.json")
-
-
+    # for img in images:
+    #     write_json(img,json_file_path)
 
     # print(images)
     print("\nURLS: ")
@@ -67,7 +67,7 @@ def get_data(url):
     return r.text
 
 
-def find_page_images(url):
+def find_page_images(url, depth):
     """
     Given a url, find all images included inside it's body
     :param url: String - the url to scan
@@ -77,14 +77,13 @@ def find_page_images(url):
     html_data = get_data(url)
     soup = BeautifulSoup(html_data, 'html.parser') # Parsing the response data, using BS
 
-
     # Getting all images on the passed link
     for item in soup.find_all('img'):
         current_image = {}
         current_image["imageUrl"] = item['src'] # add the current img url
         # print(item['src'])
         current_image["sourceUrl"] = url
-        current_image["depth"] = "0" # TODO: update based on the node passed by
+        current_image["depth"] = str(depth) # TODO: update based on the node passed by
         # print(current_image)
         images.append(current_image) # TODO: append to the json file
     # print(images)
@@ -134,19 +133,9 @@ def write_json(new_data, filename='results.json'):
         json.dump(file_data, file, indent=4)
 
 
-
-#
-# y = {"emp_name": "Nikhil",
-#      "email": "nikhil@geeksforgeeks.org",
-#      "job_profile": "Full Time"
-#      }
-#
-# write_json(y)
-
-
 # ---------- Main crawler functions --------------
 
-def crawl_page(url, depth):
+def crawl_page(url, depth, json_file_path):
     """"""
     # init a queue for pages to crawl
     pages_to_crawl = deque()
@@ -159,10 +148,26 @@ def crawl_page(url, depth):
     pages_to_crawl.append(current_page)
 
     # run BFS as long as we are on the matching depth
-    while current_page._depth  <= depth:
+    while current_page._depth <= depth:
         current_page = pages_to_crawl.popleft() # get the current page
         if current_page._url not in my_urls_list:
-            my_urls_list.add(current_page._url) # add to my urls list
+
+            # add to my urls list
+            my_urls_list.add(current_page._url)
+
+            # add current node images:
+            images = find_page_images(current_page._url, current_page._depth)
+            for img in images:
+                write_json(img,json_file_path)
+
+            # find the current page urls:
+            current_page_urls = find_page_urls(current_page._url)
+            # append it to my queue
+            for url in current_page_urls:
+                if url not in my_urls_list:
+                    pages_to_crawl.append(current_page, PageNode(url, current_page._depth + 1))
+    
+
             print("scraping the page here")
             print("appending the new data to our results file")
     pass
