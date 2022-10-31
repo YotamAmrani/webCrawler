d3df1186b363989ea47c17d9ec8fbe2c3b323755
yoyo <yotam-ra.amrani@mail.huji.com>
2022-10-31 17:49:49 +0200

    fixed and edited the image crawling and the main BFS loop

--- crawler.py
+++ crawler.py
@@ -8,7 +8,8 @@ from bs4 import BeautifulSoup
 import requests
 import re
 
-
+# GLOBAL VARS
+json_file_path = "D:/Interviews/dataloop/results.json" # TODO: handle the first time access bug
 
 """
 useful Links:
@@ -34,24 +35,12 @@ def main():
     depth = int(args[1])
 
     # Init  local files
-    json_file_path = "D:/Interviews/dataloop/results.json"
     init_results_file(json_file_path)  # Init the results file
 
-    # run the scrapping process:
-    # images = find_page_images(url, depth)
-    # urls = find_page_urls(url)
-
     # run the main crawling function:
-    crawl_page(url, depth, json_file_path)
+    crawl_page(url, depth)
 
     # Testing section:
-    print("IMAGES: ")
-    # for img in images:
-    #     write_json(img,json_file_path)
-
-    # print(images)
-    print("\nURLS: ")
-    # print(urls)
 
 # ---------- Parsing HTML functions --------------
 
@@ -65,27 +54,26 @@ def get_data(url):
     return r.text
 
 
-def find_page_images(url, depth):
+def find_page_images(pageNode):
     """
-    Given a url, find all images included inside it's body
-    :param url: String - the url to scan
-    :return: a list of all images inside of it
+    Given a url, find all images included inside it's body and append them to the results file
+    :param pageNode: A PageNode holding the current page depth and url
+    :return: None
     """
-    images = []
-    html_data = get_data(url)
+    html_data = get_data(pageNode._url)
     soup = BeautifulSoup(html_data, 'html.parser') # Parsing the response data, using BS
 
     # Getting all images on the passed link
     for item in soup.find_all('img'):
-        current_image = {}
-        current_image["imageUrl"] = item['src'] # add the current img url
-        # print(item['src'])
-        current_image["sourceUrl"] = url
-        current_image["depth"] = str(depth) # TODO: update based on the node passed by
-        # print(current_image)
-        images.append(current_image) # TODO: append to the json file
-    # print(images)
-    return images
+        if item.has_attr('src'):
+            current_image = {}
+            current_image["imageUrl"] = item['src'] # add the current img url
+            current_image["sourceUrl"] = pageNode._url
+            current_image["depth"] = str(pageNode._depth) # TODO: update based on the node passed by
+            # print(current_image)
+
+            # images.append(current_image) # TODO: append to the json file
+            write_json(current_image, json_file_path)
 
 
 def find_page_urls(url):
@@ -119,7 +107,12 @@ def init_results_file(filename):
 
 
 def write_json(new_data, filename='results.json'):
-    """"""
+    """
+    Help function, appending new data to the given json file
+    :param new_data: the data to append
+    :param filename: the json file path
+    :return: None
+    """
     with open(filename, 'r+') as file:
         # First we load existing data into a dict.
         file_data = json.load(file)
@@ -134,7 +127,7 @@ def write_json(new_data, filename='results.json'):
 # ---------- Main crawler functions --------------
 
 # TODO: to be completed
-def crawl_page(url, depth, json_file_path):
+def crawl_page(url, depth):
     """"""
     # init a queue for pages to crawl
     pages_to_crawl = deque()
@@ -146,26 +139,26 @@ def crawl_page(url, depth, json_file_path):
     current_page = PageNode(url, 0)
     pages_to_crawl.append(current_page)
 
+
     # run BFS as long as we are on the matching depth
     while current_page._depth <= depth:
         current_page = pages_to_crawl.popleft() # get the current page
-        if current_page._url not in my_urls_list:
+        if current_page._url not in my_urls_list and current_page._depth <= depth:
 
             # add to my urls list
             my_urls_list.add(current_page._url)
 
             # add current node images:
-            images = find_page_images(current_page._url, current_page._depth)
-            for img in images:
-                write_json(img,json_file_path)
+            find_page_images(current_page)
 
             # find the current page urls:
             current_page_urls = find_page_urls(current_page._url)
+
             # append it to my queue
             for url in current_page_urls:
                 if url not in my_urls_list:
                     pages_to_crawl.append(PageNode(url, current_page._depth + 1))
-            print(pages_to_crawl)
+            print("Adding site to crawl: " + current_page._url + " , depth:" + str(current_page._depth))
 
 
 
